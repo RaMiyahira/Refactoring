289a7a98b540c3b25c0749068a2ae4e7b699cfa9
unknown <ramiyahira@minha.fag.edu.br>
2025-11-18 14:21:48 -0300

    Commit 2

--- src/refactoring/Customer.java
+++ src/refactoring/Customer.java
@@ -25,25 +25,10 @@ public class Customer {
      Enumeration rentals = _rentals.elements();
      String result = "Rental Record for " + getName() + "\n";
      while (rentals.hasMoreElements()) {
-        double thisAmount = 0;
+
         Rental each = (Rental) rentals.nextElement();
 
-        //determine amounts for each line
-        switch (each.getMovie().getPriceCode()) {
-           case Movie.REGULAR:
-              thisAmount += 2;
-              if (each.getDaysRented() > 2)
-                 thisAmount += (each.getDaysRented() - 2) * 1.5;
-              break;
-           case Movie.NEW_RELEASE:
-              thisAmount += each.getDaysRented() * 3;
-              break;
-           case Movie.CHILDRENS:
-              thisAmount += 1.5;
-              if (each.getDaysRented() > 3)
-                 thisAmount += (each.getDaysRented() - 3) * 1.5;
-               break;
-        }
+        double thisAmount = amountFor(each);   // <<< ALTERAÇÃO DO REFACTORING 1
 
         // add frequent renter points
         frequentRenterPoints ++;
@@ -62,5 +47,30 @@ public class Customer {
      result += "You earned " + String.valueOf(frequentRenterPoints) +
              " frequent renter points";
      return result;
+   }
+
+   // <<< MÉTODO EXTRAÍDO (NOVO)
+   private double amountFor(Rental each) {
+      double result = 0;
+
+      switch (each.getMovie().getPriceCode()) {
+         case Movie.REGULAR:
+            result += 2;
+            if (each.getDaysRented() > 2)
+               result += (each.getDaysRented() - 2) * 1.5;
+            break;
+
+         case Movie.NEW_RELEASE:
+            result += each.getDaysRented() * 3;
+            break;
+
+         case Movie.CHILDRENS:
+            result += 1.5;
+            if (each.getDaysRented() > 3)
+               result += (each.getDaysRented() - 3) * 1.5;
+            break;
+      }
+
+      return result;
    }
 }
