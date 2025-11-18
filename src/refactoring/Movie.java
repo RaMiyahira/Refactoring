a2cfbccf2f257a7d36ecbbc4148103f1f7e58d40
unknown <ramiyahira@minha.fag.edu.br>
2025-11-18 15:16:28 -0300

    Commit 11

--- src/refactoring/Movie.java
+++ src/refactoring/Movie.java
@@ -7,19 +7,31 @@ public class Movie {
   public static final int  NEW_RELEASE = 1;
 
   private String _title;
-  private int _priceCode;
+  private Price _price;   // antes era int _priceCode
 
   public Movie(String title, int priceCode) {
       _title = title;
-      _priceCode = priceCode;
+      setPriceCode(priceCode);   // usa o setPriceCode para definir o _price
   }
 
   public int getPriceCode() {
-      return _priceCode;
+      return _price.getPriceCode();
   }
 
   public void setPriceCode(int arg) {
-      _priceCode = arg;
+      switch (arg) {
+         case REGULAR:
+            _price = new RegularPrice();
+            break;
+         case CHILDRENS:
+            _price = new ChildrensPrice();
+            break;
+         case NEW_RELEASE:
+            _price = new NewReleasePrice();
+            break;
+         default:
+            throw new IllegalArgumentException("Incorrect Price Code");
+      }
   }
 
   public String getTitle (){
@@ -50,7 +62,6 @@ public class Movie {
       return result;
   }
 
-  // <<< NOVO: veio de Rental.getFrequentRenterPoints
   public int getFrequentRenterPoints(int daysRented) {
       if ((getPriceCode() == Movie.NEW_RELEASE) &&
           daysRented > 1) {
